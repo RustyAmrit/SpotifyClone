dbe8b95eaa6b149b4e4cb6ae912359bec8b760a0
Amritpal Singh <amritpal652@gmail.com>
2022-02-16 12:39:24 +0530

    Add files via upload
    initial setUp

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("Welcome to Spotify");
+
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+
+let songs = [
+    {songName: "2", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "2", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "2", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "2", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "2", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "2", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "2", filePath: "song/1.mp3", coverPath: "covers/1.jpg"}
+]
+
+
+
+
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+//Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
